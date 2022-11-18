a0a751d25545fca665c342909084982ae3ea27ab
Steve Etches <steve.etches@ukaea.uk>
2022-11-18 11:04:37 +0000

    Used flake 8 to do style edits

--- tests/test_serializers.py
+++ tests/test_serializers.py
@@ -1,10 +1,13 @@
 from inflammation import models, serializers
 
+
 def test_patients_json_serializer():
     # Create some test data
     patients = [
-        models.Patient('Alice', [models.Observation(i, i + 1) for i in range(3)]),
-        models.Patient('Bob', [models.Observation(i, 2 * i) for i in range(3)]),
+        models.Patient('Alice', [models.Observation(i, i + 1)
+                       for i in range(3)]),
+        models.Patient('Bob', [models.Observation(i, 2 * i)
+                       for i in range(3)]),
     ]
 
     # Save and reload the data
@@ -16,6 +19,7 @@ def test_patients_json_serializer():
     for patient_new, patient in zip(patients_new, patients):
         assert patient_new.name == patient.name
 
-        for obs_new, obs in zip(patient_new.observations, patient.observations):
+        for obs_new, obs in zip(patient_new.observations,
+                                patient.observations):
             assert obs_new.day == obs.day
             assert obs_new.value == obs.value
